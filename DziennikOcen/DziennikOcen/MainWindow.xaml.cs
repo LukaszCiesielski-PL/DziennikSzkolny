83565ba2d3ba0041918c8fd6a9aa6fbec825b99e
Łukasz Ciesielski <61733177+LukaszCiesielski-PL@users.noreply.github.com>
2021-02-17 21:48:59 +0100

    Test pól logowania
    Test poprawności działania pól logowania
    Drobne poprawki graficzne

--- DziennikOcen/DziennikOcen/MainWindow.xaml.cs
+++ DziennikOcen/DziennikOcen/MainWindow.xaml.cs
@@ -27,9 +27,31 @@ namespace DziennikOcen
 
         private void zalogujBtn_Click(object sender, RoutedEventArgs e)
         {
+            string uzytkownik = this.nazwaUzytkownika.Text;
+            string haslo = this.hasloUzytkownika.Password;
 
+            if (SprawdzNazweiHaslo(uzytkownik, haslo))
+            {
+                MessageBox.Show("Jest w systemie", "Zaalogowano");
+                
+            }
+            else
+            {
+                MessageBox.Show("Niepoprawna nazwa użytkownika lub hasło", "Błąd logowania");
+                return;
+            }
+        }
+        public bool SprawdzNazweiHaslo(string uzytkownik, string haslo)
+        {
+            if (uzytkownik == "Login" & haslo == "Hasło")
+                return true;
+            else
+                return false;
         }
 
-        
+        private void nazwaUzytkownika_TextChanged(object sender, TextChangedEventArgs e)
+        {
+
+        }
     }
 }
