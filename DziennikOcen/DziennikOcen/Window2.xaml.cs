f1779d2d7cc35bea6ff359c912846878295c0896
Łukasz Ciesielski <61733177+LukaszCiesielski-PL@users.noreply.github.com>
2021-02-18 16:10:52 +0100

    Menu ucznia i nauczyciela
    poprawki

--- DziennikOcen/DziennikOcen/Window2.xaml.cs
+++ DziennikOcen/DziennikOcen/Window2.xaml.cs
@@ -17,6 +17,10 @@ namespace DziennikOcen
     /// </summary>
     public partial class Window2 : Window
     {
+        private twojeDaneNauczyciel _twojeDaneNauczyciel = new twojeDaneNauczyciel();
+        private planZajecNauczyciel _planZajecNauczyciel = new planZajecNauczyciel();
+        private ocenyNauczyciel _ocenaNauczyciel = new ocenyNauczyciel();
+        private uczniowie _uczniowie = new uczniowie();
         public Window2()
         {
             InitializeComponent();
@@ -24,22 +28,26 @@ namespace DziennikOcen
 
         private void Button_Click(object sender, RoutedEventArgs e)
         {
-
+            _twojeDaneNauczyciel = new twojeDaneNauczyciel();
+            NavigateNauczyciel.Navigate(_twojeDaneNauczyciel);
         }
 
         private void Button_Click_1(object sender, RoutedEventArgs e)
         {
-
+            _planZajecNauczyciel = new planZajecNauczyciel();
+            NavigateNauczyciel.Navigate(_planZajecNauczyciel);
         }
 
         private void Button_Click_2(object sender, RoutedEventArgs e)
         {
-
+            _ocenaNauczyciel = new ocenyNauczyciel();
+            NavigateNauczyciel.Navigate(_ocenaNauczyciel);
         }
 
         private void Button_Click_3(object sender, RoutedEventArgs e)
         {
-
+            _uczniowie = new uczniowie();
+            NavigateNauczyciel.Navigate(_uczniowie);
         }
 
         private void wylogujBtn_Click(object sender, RoutedEventArgs e)
